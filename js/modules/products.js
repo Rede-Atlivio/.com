9e4273245defec7fbba8f21598e4cdf1932e2895
painel-financeiro-borges <contatogilborges@gmail.com>
2026-01-22 10:12:49 -0300

    Create products.js

---
+++ js/modules/products.js
@@ -0,0 +1,7 @@
+// MÓDULO DE LOJA (FUTURO)
+// Aqui vamos listar produtos físicos e infoprodutos.
+
+export async function carregarProdutos() {
+    console.log("Módulo de Loja carregado. Aguardando implementação da Fase 4.");
+    // No futuro: Busca na coleção 'produtos' e renderiza cards de compra.
+}
