5a1f104cae13192e0aee51f7eda83db98c532096
painel-financeiro-borges <contatogilborges@gmail.com>
2026-02-19 14:58:50 -0300

    Update chat.js

--- js/modules/chat.js
+++ js/modules/chat.js
@@ -373,10 +373,10 @@ async function renderizarEstruturaChat(container, pedido, isProvider, orderId, s
                 
                 </div>
                 <div class="px-3 ${window.innerWidth < 768 ? 'pb-10 pt-3' : 'py-3'} flex gap-2 items-center bg-white border-t relative z-[10000]">
-                    <input type="text" id="chat-input-msg" 
-                        placeholder="${isProvider ? 'Explique como fará o serviço...' : 'Descreva o que precisa, datas e local...'}" 
-                        oninput="let uIdP = '${uidPartner}'; if(this.value.length > 0) { window.atualizarMeuStatus('online', uIdP); } clearTimeout(window.typingTimer); window.typingTimer = setTimeout(() => window.atualizarMeuStatus('online', null), 2000);"
-                        class="flex-1 bg-[#1e293b] text-white placeholder-gray-500 rounded-xl px-4 py-3 text-sm outline-none border border-slate-700 focus:border-fbbf24">
+                    <input type="text" id="chat-input-msg" 
+                        placeholder="${isProvider ? 'Explique como fará o serviço...' : 'Descreva o que precisa, datas e local...'}" 
+                        oninput="let uIdP = '${uidPartner}'; if(this.value.length > 0) { window.atualizarMeuStatus('online', uIdP); } clearTimeout(window.typingTimer); window.typingTimer = setTimeout(() => window.atualizarMeuStatus('online', null), 2000);"
+                        class="flex-1 bg-gray-100 rounded-xl px-4 py-3 text-sm outline-none border border-transparent focus:border-blue-200">
                     <button onclick="window.enviarMensagemChat('${orderId}', ${step})" class="bg-slate-900 text-white w-12 h-12 rounded-xl flex items-center justify-center shadow-lg active:scale-90 transition relative z-[1000000]">➤</button>
                 </div>
             </div>` : ''}
